8358b15467b9dbfe4057724759564ce8109867f3
Hazernit <gbcnf2012@yandex.ru>
2026-03-23 23:58:44 +0300

    Добавьте файлы проекта.

---
+++ SQLiteSample/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace SQLiteSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btConnect = new System.Windows.Forms.Button();
+            this.btRead = new System.Windows.Forms.Button();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.btCount = new System.Windows.Forms.Button();
+            this.textBoxFio = new System.Windows.Forms.TextBox();
+            this.textBoxGr = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btConnect
+            // 
+            this.btConnect.Location = new System.Drawing.Point(652, 157);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(75, 23);
+            this.btConnect.TabIndex = 0;
+            this.btConnect.Text = "Connect";
+            this.btConnect.UseVisualStyleBackColor = true;
+            this.btConnect.Click += new System.EventHandler(this.btConnect_Click);
+            // 
+            // btRead
+            // 
+            this.btRead.Location = new System.Drawing.Point(652, 186);
+            this.btRead.Name = "btRead";
+            this.btRead.Size = new System.Drawing.Size(75, 23);
+            this.btRead.TabIndex = 1;
+            this.btRead.Text = "Read";
+            this.btRead.UseVisualStyleBackColor = true;
+            this.btRead.Click += new System.EventHandler(this.btRead_Click);
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(652, 216);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(75, 23);
+            this.btAdd.TabIndex = 2;
+            this.btAdd.Text = "Add";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // btCount
+            // 
+            this.btCount.Location = new System.Drawing.Point(652, 246);
+            this.btCount.Name = "btCount";
+            this.btCount.Size = new System.Drawing.Size(75, 23);
+            this.btCount.TabIndex = 3;
+            this.btCount.Text = "Count";
+            this.btCount.UseVisualStyleBackColor = true;
+            this.btCount.Click += new System.EventHandler(this.btCount_Click);
+            // 
+            // textBoxFio
+            // 
+            this.textBoxFio.Location = new System.Drawing.Point(45, 86);
+            this.textBoxFio.Name = "textBoxFio";
+            this.textBoxFio.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFio.TabIndex = 4;
+            // 
+            // textBoxGr
+            // 
+            this.textBoxGr.Location = new System.Drawing.Point(307, 86);
+            this.textBoxGr.Name = "textBoxGr";
+            this.textBoxGr.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGr.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ФИО";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(304, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Группа";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(45, 125);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(579, 280);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxGr);
+            this.Controls.Add(this.textBoxFio);
+            this.Controls.Add(this.btCount);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.btRead);
+            this.Controls.Add(this.btConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.Button btRead;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Button btCount;
+        private System.Windows.Forms.TextBox textBoxFio;
+        private System.Windows.Forms.TextBox textBoxGr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ SQLiteSample/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SQLite;
+using System.Data;
+using System.IO;
+
+namespace SQLiteSample
+{
+    public partial class Form1 : Form
+    {
+        string dbFileName = "students.db";
+        SQLiteConnection m_dbConn;
+        SQLiteCommand m_sqlCmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btConnect_Click(object sender, EventArgs e)
+        {
+            m_dbConn = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+            m_dbConn.Open();
+            m_sqlCmd = new SQLiteCommand(m_dbConn);
+
+            MessageBox.Show("Connected");
+        }
+
+        private void btRead_Click(object sender, EventArgs e)
+        {
+
+            if (m_dbConn == null)
+            {
+                MessageBox.Show("Сначала нажми Connect");
+                return;
+            }
+
+            DataTable dTable = new DataTable();
+            string sqlQuery = "SELECT * FROM student";
+
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(sqlQuery, m_dbConn);
+            adapter.Fill(dTable);
+
+            dataGridView1.DataSource = dTable;
+
+            //DataTable dTable = new DataTable();
+            //string sqlQuery = "SELECT * FROM student";
+
+            //SQLiteDataAdapter adapter = new SQLiteDataAdapter(sqlQuery, m_dbConn);
+            //adapter.Fill(dTable);
+
+            //dataGridView1.DataSource = dTable;
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+
+            if (m_dbConn == null || m_sqlCmd == null)
+            {
+                MessageBox.Show("Сначала нажми Connect");
+                return;
+            }
+
+            string fio = textBoxFio.Text.Trim();
+            string gr = textBoxGr.Text.Trim();
+
+            if (fio == "" || gr == "")
+            {
+                MessageBox.Show("Введите ФИО и группу");
+                return;
+            }
+
+            if (!int.TryParse(gr, out int groupId))
+            {
+                MessageBox.Show("Группа должна быть числом");
+                return;
+            }
+
+            m_sqlCmd.CommandText = $"INSERT INTO student (fio, gr) VALUES ('{fio}', {groupId})";
+            m_sqlCmd.ExecuteNonQuery();
+
+            DataTable dTable = new DataTable();
+            string sqlQuery = "SELECT * FROM student";
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(sqlQuery, m_dbConn);
+            adapter.Fill(dTable);
+            dataGridView1.DataSource = dTable;
+
+            textBoxFio.Clear();
+            textBoxGr.Clear();
+
+            MessageBox.Show("Added");
+
+            // -------------------- СНИЗУ РАБОЧИЙ ВАРИАНТ ----------------
+            //if (m_dbConn == null || m_sqlCmd == null)
+            //{
+            //    MessageBox.Show("Сначала нажми Connect");
+            //    return;
+            //}
+
+            //string fio = textBoxFio.Text.Trim();
+            //string gr = textBoxGr.Text.Trim();
+
+            //if (fio == "" || gr == "")
+            //{
+            //    MessageBox.Show("Введите ФИО и группу");
+            //    return;
+            //}
+
+            //if (!int.TryParse(gr, out int groupId))
+            //{
+            //    MessageBox.Show("Группа должна быть числом");
+            //    return;
+            //}
+
+            //m_sqlCmd.CommandText = $"INSERT INTO student (fio, gr) VALUES ('{fio}', {groupId})";
+            //m_sqlCmd.ExecuteNonQuery();
+
+            //MessageBox.Show("Added");
+            //------------------СВЕРХУ РАБОЧИЙ ВАР УПРОЩЕННЫЙ БЕЗ ВЫВОДА СРАЗУ------------
+
+            //string fio = textBoxFio.Text;
+            //string gr = textBoxGr.Text;
+
+            //m_sqlCmd.CommandText = $"INSERT INTO student (fio, gr) VALUES ('{fio}', {gr})";
+            //m_sqlCmd.ExecuteNonQuery();
+
+            //MessageBox.Show("Added");
+
+            //string fio = textBoxFio.Text.Trim();
+            //string gr = textBoxGr.Text.Trim();
+
+            //if (fio == "" || gr == "")
+            //{
+            //    MessageBox.Show("Введите ФИО и группу");
+            //    return;
+            //}
+
+            //m_sqlCmd.CommandText = $"INSERT INTO student (fio, gr) VALUES ('{fio}', {gr})";
+            //m_sqlCmd.ExecuteNonQuery();
+
+            //MessageBox.Show("Added");
+
+        }
+
+        private void btCount_Click(object sender, EventArgs e)
+        {
+            if (m_dbConn == null || m_sqlCmd == null)
+            {
+                MessageBox.Show("Сначала нажми Connect");
+                return;
+            }
+
+            m_sqlCmd.CommandText = "SELECT COUNT(*) FROM student";
+            int count = Convert.ToInt32(m_sqlCmd.ExecuteScalar());
+
+            MessageBox.Show("Количество студентов: " + count);
+        }
+    }
+}
